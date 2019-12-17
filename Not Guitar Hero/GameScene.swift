8f70abff25107f5aa7867027c2be65a58758cd80
mluke2102 <matthew.luke@unamilodge.org>
2019-12-17 11:28:38 -0500

    made Function

--- Not Guitar Hero/GameScene.swift
+++ Not Guitar Hero/GameScene.swift
@@ -17,30 +17,18 @@ class GameScene: SKScene {
     var blueButton = SKSpriteNode()
     var redNote = SKSpriteNode()
    
+    override func didMove(to view: SKView) {
+        makeNote()
+    }
+    
     func makeNote()
     {
         redNote = SKSpriteNode(imageNamed: "RedNote")
         redNote.position = CGPoint(x: 0, y: 0)
-        redNote.name = "
+        redNote.name = "RedNote"
+        addChild(redNote)
     }
     
-    func createAIPaddle()
-    {
-        aiPaddle = SKSpriteNode(color: .green, size: CGSize(width: 200, height: 50))
-        aiPaddle.position = CGPoint(x: frame.width * 0.5, y: frame.height * 0.87)
-        aiPaddle.name = "AIPaddle"
-        addChild(aiPaddle)
-        
-        aiPaddle.physicsBody = SKPhysicsBody(rectangleOf: aiPaddle.frame.size)
-        aiPaddle.physicsBody?.allowsRotation = false
-        aiPaddle.physicsBody?.friction = 0
-        aiPaddle.physicsBody?.affectedByGravity = false
-        aiPaddle.physicsBody?.isDynamic = false
-        aiPaddle.physicsBody?.restitution = 1.1
-        
-        run( SKAction.repeatForever(
-                SKAction.sequence([SKAction.run(followBall),SKAction.wait(forDuration: 0.3)])
-            ))
-    }
+    
     
 }
