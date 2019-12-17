f691bbc19703d0f244980a5e56ac06f1dd8f1a28
mluke2102 <matthew.luke@unamilodge.org>
2019-12-17 14:17:09 -0500

    lots of things

--- Not Guitar Hero/GameScene.swift
+++ Not Guitar Hero/GameScene.swift
@@ -20,6 +20,7 @@ class GameScene: SKScene {
     var greenNote = SKSpriteNode()
     var blueNote = SKSpriteNode()
     var timer: Timer?
+    var deleteTrigger = SKSpriteNode()
     
     
     override func didMove(to view: SKView) {
@@ -27,15 +28,14 @@ class GameScene: SKScene {
         yellowButton.physicsBody?.categoryBitMask = 2
         greenButton.physicsBody?.categoryBitMask = 3
         blueButton.physicsBody?.categoryBitMask = 4
-        
+        deleteTrigger.physicsBody?.categoryBitMask = 5
         
         
         makeNote()
         
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(makeNote), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(makeNote), userInfo: nil, repeats: true)
         
     }
-    
     
     
     @objc func makeNote()
@@ -49,52 +49,72 @@ class GameScene: SKScene {
             redNote.position = CGPoint(x: -270, y: 800)
             redNote.name = "RedNote"
             addChild(redNote)
+            
+            let moveDown = SKAction.moveTo(y: -800, duration: 1)
+            let removeSprite = SKAction.run {
+                self.redNote.removeFromParent()
+            }
+            let sequence = SKAction.sequence([moveDown, removeSprite])
+            
+            redNote.run(sequence)
 
         }
         else if number == 2
         {
-           yellowNote = SKSpriteNode(imageNamed: "RedNote")
+           yellowNote = SKSpriteNode(imageNamed: "YellowNote")
            yellowNote.size = CGSize(width: 160, height: 160)
-           yellowNote.position = CGPoint(x: -270, y: 800)
-           yellowNote.name = "RedNote"
+           yellowNote.position = CGPoint(x: -90, y: 800)
+           yellowNote.name = "YellowNote"
            addChild(yellowNote)
+            
+            let moveDown = SKAction.moveTo(y: -800, duration: 1)
+            let removeSprite = SKAction.run {
+                self.yellowNote.removeFromParent()
+            }
+            let sequence = SKAction.sequence([moveDown, removeSprite])
+            
+            yellowNote.run(sequence)
 
         }
         else if number == 3
         {
-            greenNote = SKSpriteNode(imageNamed: "RedNote")
+            greenNote = SKSpriteNode(imageNamed: "GreenNote")
             greenNote.size = CGSize(width: 160, height: 160)
-            greenNote.position = CGPoint(x: -270, y: 800)
-            greenNote.name = "RedNote"
+            greenNote.position = CGPoint(x: 90, y: 800)
+            greenNote.name = "GreenNote"
             addChild(greenNote)
+            
+            let moveDown = SKAction.moveTo(y: -800, duration: 1)
+            let removeSprite = SKAction.run {
+                self.greenNote.removeFromParent()
+            }
+            let sequence = SKAction.sequence([moveDown, removeSprite])
+            
+            greenNote.run(sequence)
 
         }
         else if number == 4
         {
-            blueNote = SKSpriteNode(imageNamed: "RedNote")
+            blueNote = SKSpriteNode(imageNamed: "BlueNote")
             blueNote.size = CGSize(width: 160, height: 160)
-            blueNote.position = CGPoint(x: -270, y: 800)
-            blueNote.name = "RedNote"
-            addChild(redNote)
+            blueNote.position = CGPoint(x: 270, y: 800)
+            blueNote.name = "BlueNote"
+            addChild(blueNote)
+            
+            let moveDown = SKAction.moveTo(y: -800, duration: 1)
+            let removeSprite = SKAction.run {
+                self.blueNote.removeFromParent()
+            }
+            let sequence = SKAction.sequence([moveDown, removeSprite])
+            
+            blueNote.run(sequence)
 
         }
         
         
-        redNote = SKSpriteNode(imageNamed: "RedNote")
-        redNote.size = CGSize(width: 160, height: 160)
-        redNote.position = CGPoint(x: -270, y: 800)
-        redNote.name = "RedNote"
-        addChild(redNote)
         
         
-        let moveDown = SKAction.moveTo(y: -800, duration: 3)
-        let removeSprite = SKAction.run {
-            self.redNote.removeFromParent()
-        }
-        let sequence = SKAction.sequence([moveDown, removeSprite])
-        
-        redNote.run(sequence)
-        
+      
     }
     
     
