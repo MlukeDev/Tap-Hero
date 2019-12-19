c10895dfb5e7f525506b00fd8c0be5dd369af723
mluke2102 <matthew.luke@unamilodge.org>
2019-12-19 11:07:25 -0500

    new stuff

--- Not Guitar Hero/GameScene.swift
+++ Not Guitar Hero/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var redButton = SKSpriteNode()
     var yellowButton = SKSpriteNode()
@@ -23,20 +23,73 @@ class GameScene: SKScene {
     var deleteTrigger = SKSpriteNode()
     
     
+    
     override func didMove(to view: SKView) {
+        
+        self.physicsWorld.contactDelegate = self
+        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        borderBody.friction = 0.0
+        self.physicsBody = borderBody
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
+        
         redButton.physicsBody?.categoryBitMask = 1
-        yellowButton.physicsBody?.categoryBitMask = 2
-        greenButton.physicsBody?.categoryBitMask = 3
-        blueButton.physicsBody?.categoryBitMask = 4
-        deleteTrigger.physicsBody?.categoryBitMask = 5
+        yellowButton.physicsBody?.categoryBitMask = 1
+        greenButton.physicsBody?.categoryBitMask = 1
+        blueButton.physicsBody?.categoryBitMask = 1
+        redNote.physicsBody?.categoryBitMask = 2
+        yellowNote.physicsBody?.categoryBitMask = 2
+        greenNote.physicsBody?.categoryBitMask = 2
+        blueNote.physicsBody?.categoryBitMask = 2
+        deleteTrigger.physicsBody?.categoryBitMask = 3
+        
+        redNote.physicsBody?.contactTestBitMask = 3
+        yellowNote.physicsBody?.contactTestBitMask = 3
+        greenNote.physicsBody?.contactTestBitMask = 3
+        blueNote.physicsBody?.contactTestBitMask = 3
         
         
-        makeNote()
+        
+        //        set the size and position of the node
+        
+        makeDeleteTrigger()
         
         timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(makeNote), userInfo: nil, repeats: true)
         
     }
     
+    func didBegin(_ contact: SKPhysicsContact) {
+        
+        print(contact.contactPoint)
+        
+    if (contact.bodyA.categoryBitMask == 2 && contact.bodyB.categoryBitMask == 3) || (contact.bodyB.categoryBitMask == 2 && contact.bodyA.categoryBitMask == 3)
+      {
+     
+        if contact.bodyA.node == deleteTrigger || contact.bodyB.node == deleteTrigger
+        {
+            print("touched")
+        }
+        else
+        {
+            print("touched2")
+        }
+        
+      }
+       
+        
+        
+    }
+    
+    func makeDeleteTrigger()
+    {
+        deleteTrigger = SKSpriteNode(color: .red, size: CGSize(width: frame.width, height: 50))
+        deleteTrigger.position = CGPoint(x: 0, y: -640)
+        deleteTrigger.physicsBody = SKPhysicsBody(rectangleOf: deleteTrigger.frame.size)
+        deleteTrigger.physicsBody!.isDynamic = false
+        deleteTrigger.name = "DeleteTrigger"
+        addChild(deleteTrigger)
+        print("Delete Trigger Made")
+    }
     
     @objc func makeNote()
     {
@@ -48,11 +101,15 @@ class GameScene: SKScene {
             redNote.size = CGSize(width: 160, height: 160)
             redNote.position = CGPoint(x: -270, y: 800)
             redNote.name = "RedNote"
+            
             addChild(redNote)
+            
+            redNote.physicsBody = SKPhysicsBody(rectangleOf: redNote.frame.size)
+            redNote.physicsBody?.usesPreciseCollisionDetection = true
             
             let moveDown = SKAction.moveTo(y: -800, duration: 1)
             let removeSprite = SKAction.run {
-                self.redNote.removeFromParent()
+ //               self.redNote.removeFromParent()
             }
             let sequence = SKAction.sequence([moveDown, removeSprite])
             
@@ -61,15 +118,19 @@ class GameScene: SKScene {
         }
         else if number == 2
         {
-           yellowNote = SKSpriteNode(imageNamed: "YellowNote")
-           yellowNote.size = CGSize(width: 160, height: 160)
-           yellowNote.position = CGPoint(x: -90, y: 800)
-           yellowNote.name = "YellowNote"
-           addChild(yellowNote)
+            yellowNote = SKSpriteNode(imageNamed: "YellowNote")
+            yellowNote.size = CGSize(width: 160, height: 160)
+            yellowNote.position = CGPoint(x: -90, y: 800)
+            yellowNote.name = "YellowNote"
+
+            addChild(yellowNote)
+            
+           yellowNote.physicsBody = SKPhysicsBody(rectangleOf: yellowNote.frame.size)
+           yellowNote.physicsBody?.usesPreciseCollisionDetection = true
             
             let moveDown = SKAction.moveTo(y: -800, duration: 1)
             let removeSprite = SKAction.run {
-                self.yellowNote.removeFromParent()
+//                self.yellowNote.removeFromParent()
             }
             let sequence = SKAction.sequence([moveDown, removeSprite])
             
@@ -82,11 +143,15 @@ class GameScene: SKScene {
             greenNote.size = CGSize(width: 160, height: 160)
             greenNote.position = CGPoint(x: 90, y: 800)
             greenNote.name = "GreenNote"
-            addChild(greenNote)
+
+                       addChild(greenNote)
+            
+            greenNote.physicsBody = SKPhysicsBody(rectangleOf: greenNote.frame.size)
+            greenNote.physicsBody?.usesPreciseCollisionDetection = true
             
             let moveDown = SKAction.moveTo(y: -800, duration: 1)
             let removeSprite = SKAction.run {
-                self.greenNote.removeFromParent()
+//                self.greenNote.removeFromParent()
             }
             let sequence = SKAction.sequence([moveDown, removeSprite])
             
@@ -99,11 +164,15 @@ class GameScene: SKScene {
             blueNote.size = CGSize(width: 160, height: 160)
             blueNote.position = CGPoint(x: 270, y: 800)
             blueNote.name = "BlueNote"
+
             addChild(blueNote)
+            
+            blueNote.physicsBody = SKPhysicsBody(rectangleOf: blueNote.frame.size)
+            blueNote.physicsBody?.usesPreciseCollisionDetection = true
             
             let moveDown = SKAction.moveTo(y: -800, duration: 1)
             let removeSprite = SKAction.run {
-                self.blueNote.removeFromParent()
+//                self.blueNote.removeFromParent()
             }
             let sequence = SKAction.sequence([moveDown, removeSprite])
             
