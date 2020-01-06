a1fc5773989a844e8259915bdef741122031af01
NextCarey <hashcow12@gmail.com>
2020-01-06 11:45:06 -0500

    title screen

--- Not Guitar Hero/TitleScreen.swift
+++ Not Guitar Hero/TitleScreen.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import SpriteKit
+import GameplayKit
+
+class TitleScreen: SKScene, SKPhysicsContactDelegate {
+    var startButton = SKLabelNode()
+    
+    override func didMove(to view: SKView) {
+        startButton = self.childNode(withName: "startButton") as! SKLabelNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+         let location = touches.first?.location(in: self)
+        if startButton.frame.contains(location!)
+        {
+            
+            let gameScene = GameScene(fileNamed: "GameScene")
+            gameScene?.scaleMode = .aspectFill
+            let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+            view?.presentScene(gameScene!, transition: reveal)
+
+            
+        }
+
+    }
+    
+}
+
